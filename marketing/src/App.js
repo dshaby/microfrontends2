941c2e85b7bb7e286ca4fc4eed94d7ad8a8d5889
Shaby <88353394+dshaby@users.noreply.github.com>
2024-01-01 11:16:27 -0800

    add unique microfrontend classnames

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma', // ma = marketing app this is to avoid css class name collision
+  //it adds the 'ma' prefix to all the css class names
+});
+
 export default () => {
   return (
-    <>
-      <h1>Marketing microfrontend!</h1>
-      <div>
-        <StylesProvider>
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/pricing" component={Pricing} />
-              <Route exact path="/" component={Landing} />
-            </Switch>
-          </BrowserRouter>
-        </StylesProvider>
-      </div>
-    </>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route exact path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
